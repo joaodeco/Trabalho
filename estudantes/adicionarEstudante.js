79260a06430b9ab4d802f397a331e452a7596049
Murilo Mergner Borges <murilomergner@gmail.com>
2024-09-09 15:05:07 -0300

    Update adicionarEstudante.js

--- estudantes/adicionarEstudante.js
+++ estudantes/adicionarEstudante.js
@@ -1,18 +1,16 @@
 const estudantes = require('../data');
 
-function criarEstudante(req, res) {
-console.log(req.body);
-const novoEstudante = {
-id: estudantes.length + 1,
-nome: req.body.titulo,
-matricula: req.body.matricula,
-curso: req.body.curso,
-ano: req.body.curso
-};
-estudantes.push(novoEstudante)
-res
-.status(201)
-.send({message: 'Baralho criado com sucesso!', estudantes: novoEstudante});
+  function criarEstudante(req, res) {
+  console.log(req.body);
+    const novoEstudante = {
+    id: estudantes.length + 1,
+    nome: req.body.titulo,
+    matricula: req.body.matricula,
+    curso: req.body.curso,
+    ano: req.body.curso
+    };
+  estudantes.push(novoEstudante)
+  res.status(201).send({message: 'Estudante criado com sucesso!', estudantes: novoEstudante});
 }
 
 module.exports = criarEstudante
